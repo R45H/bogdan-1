ea15969f807e7708bbffe2431ce5df5cd5e31d46
DoretskiyBV <workbd@bk.ru>
2022-01-28 17:10:46 +0300

    fixed js and classes, add burger in uikit.html

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,11 @@
 window.addEventListener('load', function () {
-    console.log('Страница полностью загружена');
+    console.log('Страница полностью загружена')
     let NavBurger = document.querySelector('.nav-burger');
-    let BurgerIcon = document.querySelector('.nav-burger__icon');
-
     NavBurger.onclick = function () {
-        console.log('Нажата иконка меню');
-        if (BurgerIcon.classList.contains('nav-burger__icon_on')) {
-            BurgerIcon.classList.remove('nav-burger__icon_on');
+        if (NavBurger.classList.contains('nav-burger_on')) {
+            NavBurger.classList.remove('nav-burger_on')
         } else {
-            BurgerIcon.classList.add('nav-burger__icon_on');
+            NavBurger.classList.add('nav-burger_on')
         }
-    };
+    }
 });
